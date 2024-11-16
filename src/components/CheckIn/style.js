1e06edd76166c74b1f72f07d74a1961f0692948c
junjuny0227 <junjuny0227@gmail.com>
2024-11-17 02:16:23 +0900

    feat: finish CheckIn component

---
+++ src/components/CheckIn/style.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 56px 16px 56px 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Title = styled.h1`
+  color: #59ff00;
+  font-size: 36px;
+  font-weight: 900;
+  line-height: 45px;
+  font-family: SUIT;
+`;
+
+export const Time = styled.p`
+  font-size: 64px;
+  font-weight: 800;
+  line-height: 80px;
+  text-align: left;
+`;
+
+export const MyStatus = styled.div`
+  display: flex;
+  width: 27.75rem;
+  height: 80px;
+  padding: 27px 24px;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+  background: #252525;
+`;
+
+export const RoomNumber = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 4px;
+`;
+
+export const Text = styled.p`
+  color: #fff;
+  font-size: ${({ fontSize }) => fontSize || "16px"};
+  font-weight: ${({ fontWeight }) => fontWeight || "400"};
+`;
