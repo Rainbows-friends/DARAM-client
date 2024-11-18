9349a8f029998c5ab7125b68a7b872aff923d4b5
junjuny0227 <junjuny0227@gmail.com>
2024-11-18 17:54:00 +0900

    style: set overflow hidden

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const Wrapper = styled.main`
   width: 100%;
-  height: 1992px;
+  height: 1923px;
   background-color: #111214;
   color: white;
   display: flex;
   justify-content: center;
   align-items: start;
   position: relative;
+  overflow: hidden;
 `;
 
 export const Container = styled.div`
