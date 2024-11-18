8ef0a516b5afba72ee35410cd0be85e0290b101e
junjuny0227 <junjuny0227@gmail.com>
2024-11-18 16:46:58 +0900

    feat: translucent header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -7,7 +7,8 @@ export const Wrapper = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #111214;
+  backdrop-filter: blur(10px);
+  background-color: rgba(17, 18, 20, 0.2);
   position: sticky;
   top: 0;
   z-index: 1;
