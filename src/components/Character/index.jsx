07ba6b763e07b63126b9274fa6ac52bf85915156
junjuny0227 <junjuny0227@gmail.com>
2024-11-22 12:08:16 +0900

    feat: complete character component

--- src/components/Character/index.jsx
+++ src/components/Character/index.jsx
@@ -1,11 +1,24 @@
-// Character.js
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as S from "./style";
 
-const Character = ({ src, alt }) => {
+const Character = ({ src, alt, initialTop, initialLeft }) => {
+  const remToPx = (rem) =>
+    parseFloat(rem) *
+    parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+  const initialX =
+    typeof initialLeft === "string" && initialLeft.includes("rem")
+      ? remToPx(initialLeft)
+      : parseFloat(initialLeft) || 0;
+
+  const initialY =
+    typeof initialTop === "string" && initialTop.includes("rem")
+      ? remToPx(initialTop)
+      : parseFloat(initialTop) || 0;
+
   const [isDragging, setIsDragging] = useState(false);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: initialX, y: initialY });
 
   const handleMouseDown = (e) => {
     setIsDragging(true);
@@ -28,8 +41,25 @@ const Character = ({ src, alt }) => {
     setIsDragging(false);
   };
 
+  // window 이벤트 등록 및 해제
+  useEffect(() => {
+    if (isDragging) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    }
+
+    // 컴포넌트 언마운트 시 이벤트 해제
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging]);
+
   const handleDragStart = (e) => {
-    e.preventDefault(); // 기본 드래그 이벤트 비활성화
+    e.preventDefault();
   };
 
   return (
@@ -43,9 +73,6 @@ const Character = ({ src, alt }) => {
         cursor: isDragging ? "grabbing" : "grab",
       }}
       onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp}
       onDragStart={handleDragStart} // 드래그 이벤트 비활성화
     />
   );
