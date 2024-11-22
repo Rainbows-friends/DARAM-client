4cc126af8e460e662eecb3a88b8c3eacbb8147f2
junjuny0227 <junjuny0227@gmail.com>
2024-11-22 23:40:43 +0900

    feat: set props

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -4,9 +4,9 @@ import * as S from "./style";
 function Dashboard() {
   return (
     <S.Wrapper>
-      <InfoSection text="기숙사 입실 현황" />
-      <InfoSection text="미입실자 현황" />
-      <InfoSection text="공지사항" />
+      <InfoSection text="기숙사 입실 현황" path="/checkin" />
+      <InfoSection text="미입실자 현황" path="/noshow" />
+      <InfoSection text="공지사항" path="/notice" />
     </S.Wrapper>
   );
 }
--- src/components/InfoSection/index.jsx
+++ src/components/InfoSection/index.jsx
@@ -1,9 +1,9 @@
 import ArrowIcon from "../../asset/arrow.svg";
 import * as S from "./style";
 
-function InfoSection({ text }) {
+function InfoSection({ text, path }) {
   return (
-    <S.Wrapper>
+    <S.Wrapper to={path}>
       <S.Text>{text}</S.Text>
       <img src={ArrowIcon} alt="Arrow Icon" />
     </S.Wrapper>
