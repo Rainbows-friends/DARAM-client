f49ef0ce95af4cf0f777bbef980b349da9502670
junjuny0227 <junjuny0227@gmail.com>
2024-11-25 13:26:51 +0900

    chore: setup absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@layout", replacement: "/src/layout" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@styles", replacement: "/src/styles" },
+    ],
+  },
 });
