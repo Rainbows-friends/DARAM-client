fe402578d449ee1aad247c57ce54a0b63229493a
junjuny0227 <junjuny0227@gmail.com>
2024-11-27 14:38:42 +0900

    feat: display time from props

--- src/components/CheckIn/index.jsx
+++ src/components/CheckIn/index.jsx
@@ -1,24 +1,28 @@
 import * as S from "./style";
 
-function CheckIn() {
+function CheckIn({ time, loading, error }) {
+  const renderTime = () => {
+    if (loading) return "로딩 중...";
+    if (error) return "에러...";
+    if (!time) return "시간 정보 없음";
+    return `${time.hours}h${time.minutes}m${time.seconds}s`;
+  };
+
   return (
     <S.Wrapper>
       <div>
         <S.Title>DARAM</S.Title>
-        <S.Time>14m03s</S.Time>
+        <S.Time>{renderTime()}</S.Time>
       </div>
       <S.MyStatus>
         <S.RoomNumber>
-          <S.Text fontSize="20px" fontWeight="600">
-            1416 전준연
-          </S.Text>
-          <S.Text fontSize="14px" fontWeight="400">
-            305호
+          <S.Text fontSize="20px" fontWeight="500">
+            다람과 함께 하고 싶다면?
           </S.Text>
         </S.RoomNumber>
-        <S.Text fontSize="16px" fontWeight="400">
-          나의 지각 횟수: n회
-        </S.Text>
+        <S.LoginBtn>
+          <S.Text fontWeight="600">로그인</S.Text>
+        </S.LoginBtn>
       </S.MyStatus>
     </S.Wrapper>
   );
