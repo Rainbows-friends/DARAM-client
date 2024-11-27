22822ce8af3ecf48c4278ae7b3e28ff5e6652a72
junjuny0227 <junjuny0227@gmail.com>
2024-11-27 15:11:30 +0900

    feat: create circleData

---
+++ src/data/circleData.js
@@ -0,0 +1,39 @@
+// circleData.js
+export const circleData = [
+  {
+    size: "25rem",
+    color: "linear-gradient(152deg, #70E249 15.9%, #477E34 90.38%)",
+    top: "1.625rem",
+    left: "9.375rem",
+  },
+  {
+    size: "6.25rem",
+    color: "linear-gradient(152deg, #49E2CB 15.9%, #347E5F 90.38%)",
+    top: "7.1875rem",
+    left: "51.5rem",
+  },
+  {
+    size: "12.5rem",
+    color: "linear-gradient(152deg, #E24F49 15.9%, #7E4A34 90.38%)",
+    top: "32.375rem",
+    left: "97.125rem",
+  },
+  {
+    size: "31.25rem",
+    color: "linear-gradient(152deg, #AA49E2 15.9%, #48347E 90.38%)",
+    top: "94.75rem",
+    left: "-6.25rem",
+  },
+  {
+    size: "18.75rem",
+    color: "linear-gradient(152deg, #E2CE49 15.9%, #7E5D34 90.38%)",
+    top: "53.4375rem",
+    left: "29.375rem",
+  },
+  {
+    size: "25rem",
+    color: "linear-gradient(152deg, #4970E2 15.9%, #343C7E 90.38%)",
+    top: "67.25rem",
+    left: "105rem",
+  },
+];
