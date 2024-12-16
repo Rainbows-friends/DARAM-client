1b4d7901669a145b36f3132302a9e2b982e7fd3e
junjuny0227 <junjuny0227@gmail.com>
2024-12-16 19:58:04 +0900

    FloorStatus 컴포넌트 임시 완성

---
+++ src/components/Member/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const RoomAndName = styled.p`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 25px;
+`;
+
+export const Time = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  color: #aaa;
+`;
