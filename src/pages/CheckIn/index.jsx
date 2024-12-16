161a5481374f43a16e536a5983b336f2c99797c5
junjuny0227 <junjuny0227@gmail.com>
2024-12-16 19:58:35 +0900

    CheckIn 페이지 임시 완성

--- src/pages/CheckIn/index.jsx
+++ src/pages/CheckIn/index.jsx
@@ -1,7 +1,17 @@
+import FloorStatus from "@components/FloorStatus";
 import * as S from "./style";
 
 function CheckIn() {
-  return <h1>CheckIn</h1>;
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <FloorStatus floor={2} room={201} name={"***"} time={"09:27:13 PM"} />
+        <FloorStatus floor={3} />
+        <FloorStatus floor={4} />
+        <FloorStatus floor={5} />
+      </S.Container>
+    </S.Wrapper>
+  );
 }
 
 export default CheckIn;
