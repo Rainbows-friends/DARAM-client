3b4155d56ad308e480ed7b96c4149004ea9c1c47
junjuny0227 <junjuny0227@gmail.com>
2024-12-17 17:23:12 +0900

    feat: Google Oauth url 지정

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -3,18 +3,20 @@ import google from "@assets/google.svg";
 import cancel from "@assets/cancel.svg";
 
 function LoginModal({ onClose }) {
-  // 모달 배경 클릭 시 모달을 닫는 함수
   const handleBackgroundClick = (e) => {
     if (e.target === e.currentTarget) {
       onClose();
     }
+  };
+  const handleGoogleLogin = () => {
+    window.location.href = import.meta.env.VITE_GOOGLE_OAUTH_URL;
   };
 
   return (
     <S.Wrapper onClick={handleBackgroundClick}>
       <S.Container>
         <S.Text>로그인</S.Text>
-        <S.GoogleBtn>
+        <S.GoogleBtn onClick={handleGoogleLogin}>
           <img src={google} alt="google" />
           Google 계정으로 로그인
         </S.GoogleBtn>
