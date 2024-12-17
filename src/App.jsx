2aff5e8c0d754a68f224476d1d993259a05759f5
junjuny0227 <junjuny0227@gmail.com>
2024-12-17 20:57:04 +0900

    feat: 로그인 기능

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "@pages/Home";
 import CheckIn from "@pages/CheckIn";
 import NoShow from "@pages/NoShow";
 import Notice from "@pages/Notice";
+import Callback from "@pages/Callback";
 import "@styles/global.css";
 import "@styles/fonts.css";
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/checkin" element={<CheckIn />} />
         <Route path="/noshow" element={<NoShow />} />
         <Route path="/notice" element={<Notice />} />
+        <Route path="/login/oauth2/code/google" element={<Callback />} />
       </Routes>
     </Router>
   );
