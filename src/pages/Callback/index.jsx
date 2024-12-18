dc7e9d75ec2bed84450ba26b4a5a6f097f93d74c
junjuny0227 <junjuny0227@gmail.com>
2024-12-18 09:19:52 +0900

    feat: toast 설정

--- src/pages/Callback/index.jsx
+++ src/pages/Callback/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Callback() {
   const navigate = useNavigate();
@@ -8,34 +10,35 @@ function Callback() {
   useEffect(() => {
     const sendCodeToBackend = async () => {
       const queryParams = new URLSearchParams(window.location.search);
-      const code = queryParams.get("code");
+      let code = queryParams.get("code");
 
       if (!code) {
-        console.error("code가 없습니다. 로그인 실패");
+        toast.error("로그인 코드가 없습니다. 홈으로 이동합니다.");
         navigate("/home");
         return;
       }
 
+      // URL 디코딩: %2F를 /로 변환
+      code = decodeURIComponent(code);
+
       try {
-        const response = await axios.post(
-          "https://amond-server.kro.kr/api/v2/auth/signin",
-          {
-            header: {},
-            body: { code },
-          }
-        );
+        const response = await axios.post(import.meta.env.VITE_SIGNIN_API_URL, {
+          header: {},
+          body: { code },
+        });
 
         const { token } = response.data;
 
         if (token) {
           localStorage.setItem("token", token);
+          toast.success("로그인 되었습니다.");
           navigate("/home");
         } else {
-          console.error("로그인 실패:", response.data.message);
+          toast.error("로그인 실패");
           navigate("/home");
         }
       } catch (error) {
-        console.error("서버 통신 오류:", error.response?.data || error.message);
+        toast.error("서버 통신 오류");
         navigate("/home");
       }
     };
