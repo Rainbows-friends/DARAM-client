6e9b7c574f6d083eda9a81d2b8a38db2754fbb26
xy-jxn <s24016@gsm.hs.kr>
2024-12-18 23:34:28 +0900

    feat: data 파일 추가

---
+++ src/data/circleDataTeam.js
@@ -0,0 +1,38 @@
+export const circleDataTeam = [
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #70E249 15.9%, #477E34 90.38%)",
+    left: "100px",
+    top: "521px",
+  },
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #49E2CB 15.9%, #347E5F 90.38%)",
+    left: "1290px",
+    top: "84px",
+  },
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #E24F49 15.9%, #7E4A34 90.38%)",
+    left: "676px",
+    top: "291px",
+  },
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #AA49E2 15.9%, #48347E 90.38%)",
+    top: "84px",
+    left: "351px",
+  },
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #E2CE49 15.9%, #7E5D34 90.38%)",
+    left: "1706px",
+    top: "341px",
+  },
+  {
+    size: "100px",
+    color: "linear-gradient(152deg, #4970E2 15.9%, #343C7E 90.38%)",
+    left: "1122px",
+    top: "645px",
+  },
+];
