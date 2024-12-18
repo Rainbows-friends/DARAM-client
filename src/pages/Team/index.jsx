ba1489ae3bc2283d90b67f0c0ff47a379fa7605a
xy-jxn <s24016@gsm.hs.kr>
2024-12-18 23:35:13 +0900

    feat: Top UI 완성

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,7 +1,39 @@
 import * as S from "./style";
+import Header from "../../components/Header";
+import Circle from "../../components/Circle";
+import { circleDataTeam } from "../../data/circleDataTeam";
+import downarrow from "@assets/downarrow.svg";
 
 function Team() {
-  return <h1>Team</h1>;
+  return (
+    <S.Wrapper>
+      <Header />
+      <S.Top>
+        {circleDataTeam.map((data, index) => (
+          <Circle
+            key={index}
+            size={data.size}
+            color={data.color}
+            top={data.top}
+            left={data.left}
+          />
+        ))}
+        <S.TextWrapper>
+          <S.Text style={{fontSize: "40px"}} fontSize="40px">
+            기숙사 입실 확인, 이제 <S.Green>AI 얼굴 인식</S.Green>으로 자동화!
+          </S.Text>
+          <S.Text style={{fontSize: "32px"}} fontSize="32px">
+            <S.Green>DARAM</S.Green>이 기숙사 자치위원회의 업무를 줄이고,학생들에게 편리한 기숙사 생활을 제공하겠습니다.
+          </S.Text>
+        </S.TextWrapper>
+        <img
+          src={downarrow}
+          alt=""
+          style={{ position: "absolute", top: "750px" }}
+        />
+      </S.Top>
+    </S.Wrapper>
+  );
 }
 
 export default Team;
--- src/pages/Team/style.js
+++ src/pages/Team/style.js
@@ -1,1 +1,50 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  margin-top: 73px;
+  background-color: #111214;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  position: relative;
+`;
+
+export const Top = styled.div`
+  width: 100%;
+  height: calc(100vh - 73px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Mid = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextWrapper = styled.div`
+  position: absolute;
+  top: 285px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Text = styled.p`
+  color: #FAFAFA;
+  text-align: right;
+  font-family: SUIT;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 55px;
+`;
+
+export const Green = styled.span`
+  color: #59ff00;
+`;
