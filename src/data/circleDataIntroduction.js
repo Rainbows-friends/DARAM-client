097509fa29a51e04f1aa81eb67319abca529c7ca
xy-jxn <s24016@gsm.hs.kr>
2024-12-19 12:18:46 +0900

    feat: 데이터 파일 추가

---
+++ src/data/circleDataIntroduction.js
@@ -0,0 +1,38 @@
+export const circleDataIntroduction = [
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #70E249 15.9%, #477E34 90.38%)",
+    left: "974px",
+    top: "576px",
+  },
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #49E2CB 15.9%, #347E5F 90.38%)",
+    left: "547px",
+    top: "963px",
+  },
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #E24F49 15.9%, #7E4A34 90.38%)",
+    left: "1234px",
+    top: "820px",
+  },
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #AA49E2 15.9%, #48347E 90.38%)",
+    left: "859px",
+    top: "1267px",
+  },
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #E2CE49 15.9%, #7E5D34 90.38%)",
+    left: "651px",
+    top: "178px",
+  },
+  {
+    size: "120px",
+    color: "linear-gradient(152deg, #4970E2 15.9%, #343C7E 90.38%)",
+    left: "1357px",
+    top: "312px",
+  },
+];
---
+++ src/data/introductionData.js
@@ -0,0 +1,8 @@
+export const introductionData = [
+  { name: "주여진", major: "Front-end", color: "#FFE851", top: "270px", left: "733px"},
+  { name: "전준연", major: "Front-end", color: "#5378E4", top: "372px", left: "1074px"},
+  { name: "김민솔", major: "UI/UX Design", color: "#70E249", top: "745px", left: "843px"},
+  { name: "김명현", major: "Flutter", color: "#E24F49", top: "908px", left: "1033px"},
+  { name: "이은아", major: "Back-end", color: "#49E2CB", top: "1036px", left: "611px"},
+  { name: "김태은", major: "Back-end", color: "#AA49E2", top: "1354px", left: "870px"},
+]
