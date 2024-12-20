07e10337ea79e9b98a6d0252cef1c5c359f8cb85
xy-jxn <s24016@gsm.hs.kr>
2024-12-20 11:27:13 +0900

    refactor: 요소 위치 변경

--- src/data/characterDataIntroduction.js
+++ src/data/characterDataIntroduction.js
@@ -6,10 +6,10 @@ import myunghyun from "@assets/myunghyun.svg";
 import taeeun from "@assets/taeeun.svg";
 
 export const characterDataIntroduction = [
-  { src: junyeon, alt: "Junyeon", top: "416px", left: "424.5px" },
-  { src: yeojin, alt: "yeojin", top: "385px", left: "994.5px" },
-  { src: euna, alt: "euna", top: "372px", left: "144.5px" },
-  { src: minsol, alt: "minsol", top: "372px", left: "700.5px" },
-  { src: myunghyun, alt: "myunghyun", top: "372px", left: "1585.5px" },
-  { src: taeeun, alt: "taeeun", top: "372px", left: "1295.5px" },
+  { src: junyeon, alt: "Junyeon", top: "376px", left: "424.5px" },
+  { src: yeojin, alt: "yeojin", top: "345px", left: "994.5px" },
+  { src: euna, alt: "euna", top: "332px", left: "144.5px" },
+  { src: minsol, alt: "minsol", top: "332px", left: "700.5px" },
+  { src: myunghyun, alt: "myunghyun", top: "332px", left: "1585.5px" },
+  { src: taeeun, alt: "taeeun", top: "332px", left: "1295.5px" },
 ];
