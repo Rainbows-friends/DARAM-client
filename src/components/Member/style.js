2f66c113c5b527f5d0e25204fd7a9ad5e4386735
junjuny0227 <junjuny0227@gmail.com>
2024-12-30 11:14:50 +0900

    feat: cursor 옵션 추가

--- src/components/Member/style.js
+++ src/components/Member/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  cursor: pointer;
 `;
 
 export const RoomAndName = styled.p`
